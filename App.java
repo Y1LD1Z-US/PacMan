d231bb2b950761034b956567a601c60b66c2630d
Murat <muratyldz16@icloud.com>
2025-12-05 03:15:02 -0500

    Update App.java

--- App.java
+++ App.java
@@ -1,23 +1,27 @@
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        int rowCount = 21;
-        int columnCount = 19;
-        int tileSize = 32;
-        int boardWidth = columnCount * tileSize;
-        int boardHeight = rowCount * tileSize;
+    public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Pac Man");
-        frame.setSize(boardWidth, boardHeight);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Pac-Man");
 
-        PacMan pacmanGame = new PacMan();
-        frame.add(pacmanGame);
-        frame.pack();
-        pacmanGame.requestFocus();
-        frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setResizable(false);
+
+            // Our game panel (sets its own preferred size)
+            PacMan pacmanGame = new PacMan();
+            frame.add(pacmanGame);
+
+            // Size window based on PacMan's preferred size
+            frame.pack();
+            frame.setLocationRelativeTo(null); // center on screen
+
+            frame.setVisible(true);
+
+            // Make sure Pac-Man panel gets keyboard focus
+            pacmanGame.requestFocusInWindow();
+        });
     }
 }
